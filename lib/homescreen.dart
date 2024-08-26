0abe0d97c232b604bfd8056738196ba22c1ff76e
Ayat Esaam <eayatessam1@gmail.com>
2024-08-26 10:07:59 +0300

    initialize project

---
+++ lib/homescreen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatelessWidget {
+static const String routeName = '/';
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
